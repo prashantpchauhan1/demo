feda203832f364f890c7ada83c7023d897acc998
iprashantchauhan <prashant1999chauhan@gmail.com>
2025-09-14 10:32:48 +0530

    adding config server changes

--- src/main/java/com/coding/demo/dto/ResponseDto.java
+++ src/main/java/com/coding/demo/dto/ResponseDto.java
@@ -1,16 +1,44 @@
 package com.coding.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class ResponseDto<T>{
     private HttpStatus status;
     private String message;
     private T data;
 
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public ResponseDto(HttpStatus status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+    public ResponseDto(){
+        super();
+    }
 }
--- src/main/java/com/coding/demo/model/Address.java
+++ src/main/java/com/coding/demo/model/Address.java
@@ -1,12 +1,8 @@
 package com.coding.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import org.springframework.data.annotation.Id;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Address {
     @Id
     private String addressId;
@@ -15,4 +11,65 @@ public class Address {
     private String city;
     private String pincode;
     private String state;
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Address(String addressId, String addressLine1, String addressLine2, String city, String pincode, String state) {
+        this.addressId = addressId;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.pincode = pincode;
+        this.state = state;
+    }
+
+    public Address(){
+        super();
+    }
 }
